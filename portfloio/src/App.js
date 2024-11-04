d90779b4a455e8a8b81a05eb43eaf27fc04b5d94
Shams Ur Rahman <shamsurrahman1578@gmail.com>
2024-11-04 04:24:00 -0800

    my first react

---
+++ portfloio/src/App.js
@@ -0,0 +1,16 @@
+
+import car from"./assets/images/car.jpeg"
+
+function App() {
+  return (
+    <>
+    <div>
+      <h1>FIRST CLASS OF REACT</h1>
+      <h2>Welcome</h2>
+      <img src={car} alt=""/>
+    </div>
+    </>
+  );
+}
+
+export default App;
